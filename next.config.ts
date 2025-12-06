3b410f4e96c433b5da58d72ac505b5a48fb3d1b2
JeonB <newjeonb@gmail.com>
2025-12-06 11:14:29 +0900

    Chore: 최적화 옵션 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: true,
+  output: "standalone",
+  devIndicators: false,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+  experimental: {
+    optimizePackageImports: ["lucide-react", "react-icons"],
+  },
 };
 
 export default nextConfig;
