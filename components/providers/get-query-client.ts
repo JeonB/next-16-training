e9e7574d4158a1f73d7928aad1d49f777f451b1e
JeonB <newjeonb@gmail.com>
2025-12-23 15:50:44 +0900

    쿼리프로바이더 수정

--- components/providers/get-query-client.ts
+++ components/providers/get-query-client.ts
@@ -4,6 +4,14 @@ import {
   isServer,
 } from "@tanstack/react-query";
 
+// export const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       staleTime: 60 * 1000,
+//     },
+//   },
+// });
+
 function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
@@ -27,10 +35,6 @@ export function getQueryClient() {
     // Server: always make a new query client
     return makeQueryClient();
   } else {
-    // Browser: make a new query client if we don't already have one
-    // This is very important, so we don't re-make a new client if React
-    // suspends during the initial render. This may not be needed if we
-    // have a suspense boundary BELOW the creation of the query client
     if (!browserQueryClient) browserQueryClient = makeQueryClient();
     return browserQueryClient;
   }
